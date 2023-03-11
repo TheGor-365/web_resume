2bffd1b0faca9074ab52ab8ad097ca8a4d57b2b7
Gor <7495500@gmail.com>
2023-03-11 18:34:36 +0300

    Add turbo pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def letter
+    render turbo_stream: turbo_stream.replace("toggler", partial: "pages/letter")
+  end
+
+  def projects
+    render turbo_stream: turbo_stream.replace("toggler", partial: "pages/projects")
+  end
+
+  def resume
+    render turbo_stream: turbo_stream.replace("toggler", partial: "pages/resume")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get 'letter', to: 'pages#letter', as: 'letter'
+  get 'resume', to: 'pages#resume', as: 'resume'
+  get 'projects', to: 'pages#projects', as: 'projects'
 end
