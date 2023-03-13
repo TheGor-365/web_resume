940367fd6c2110694c25cdff8da17d17392618a1
Gor <7495500@gmail.com>
2023-03-13 17:07:27 +0300

    Modify app configure for deploy 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 Certificate.create([
-  { title: 'Ruby on Rails',     image: 'ruby_on_rails_page-0001',           school: "codeacademy.com" },
+  { title: 'Ruby on Rails',     image: 'ruby_on_rails_page-0001.jpg',           school: "codeacademy.com" },
   # { title: 'Bash',              image: 'bash_page-0001',                    school: "codeacademy.com" },
-  { title: 'Bootstrap',         image: 'bootstrap_page-0001',               school: "codeacademy.com" },
-  { title: 'Code Foundation',   image: 'code_foundation_page-0001',         school: "codeacademy.com" },
-  { title: 'Git / GitHub',      image: 'git_git_hub_page-0001',             school: "codeacademy.com" },
-  { title: 'JavaScript build',  image: 'javascript_build_cert_page-0001',   school: "codeacademy.com" },
-  { title: 'JavaScript',        image: 'javascript_page-0001',              school: "codeacademy.com" },
-  { title: 'jQuery',            image: 'jQuery_cert_page-0001',             school: "codeacademy.com" },
-  { title: 'Regexp',            image: 'regexp_page-0001',                  school: "codeacademy.com" },
-  { title: 'Ruby',              image: 'ruby_cert_page-0001',               school: "codeacademy.com" },
-  { title: 'Ruby on Rails 2',   image: 'ruby_on_rails_page-0001',           school: "codeacademy.com" },
-  { title: 'SQL 2',             image: 'SQL_2_page-0001',                   school: "codeacademy.com" },
-  { title: 'SQL',               image: 'SQL_page-0001',                     school: "codeacademy.com" },
-  { title: 'CSS',               image: 'css_page-0001',                     school: "codeacademy.com" }
+  { title: 'Bootstrap',         image: 'bootstrap_page-0001.jpg',               school: "codeacademy.com" },
+  { title: 'Code Foundation',   image: 'code_foundation_page-0001.jpg',         school: "codeacademy.com" },
+  { title: 'Git / GitHub',      image: 'git_git_hub_page-0001.jpg',             school: "codeacademy.com" },
+  { title: 'JavaScript build',  image: 'javascript_build_cert_page-0001.jpg',   school: "codeacademy.com" },
+  { title: 'JavaScript',        image: 'javascript_page-0001.jpg',              school: "codeacademy.com" },
+  { title: 'jQuery',            image: 'jQuery_cert_page-0001.jpg',             school: "codeacademy.com" },
+  { title: 'Regexp',            image: 'regexp_page-0001.jpg',                  school: "codeacademy.com" },
+  { title: 'Ruby',              image: 'ruby_cert_page-0001.jpg',               school: "codeacademy.com" },
+  { title: 'Ruby on Rails 2',   image: 'ruby_on_rails_page-0001.jpg',           school: "codeacademy.com" },
+  { title: 'SQL 2',             image: 'SQL_2_page-0001.jpg',                   school: "codeacademy.com" },
+  { title: 'SQL',               image: 'SQL_page-0001.jpg',                     school: "codeacademy.com" },
+  { title: 'CSS',               image: 'css_page-0001.jpg',                     school: "codeacademy.com" }
 ])
 
 
