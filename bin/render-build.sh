8e40581e4d4055facebb07bbecaca2533f2330ab
Gor <7495500@gmail.com>
2023-03-13 22:33:44 +0300

    Modify app configure for deploy 12

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 bundle install
 yarn install
 bundle exec rake assets:clean
-bundle exec rake assets:clobber
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
