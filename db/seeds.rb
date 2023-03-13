03990423ed7ae95ccfe3e1511f2e4a02aecb4ea2
Gor <7495500@gmail.com>
2023-03-13 16:49:25 +0300

    Modify app configure for deploy 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Certificate.create([
-  { title: 'Ruby on Rails',     image: 'ruby_on_rails_page-0001',       school: "codeacademy.com" },
-  { title: 'Bash',              image: 'bash_page-0001',                    school: "codeacademy.com" },
+  { title: 'Ruby on Rails',     image: 'ruby_on_rails_page-0001',           school: "codeacademy.com" },
+  # { title: 'Bash',              image: 'bash_page-0001',                    school: "codeacademy.com" },
   { title: 'Bootstrap',         image: 'bootstrap_page-0001',               school: "codeacademy.com" },
   { title: 'Code Foundation',   image: 'code_foundation_page-0001',         school: "codeacademy.com" },
   { title: 'Git / GitHub',      image: 'git_git_hub_page-0001',             school: "codeacademy.com" },
